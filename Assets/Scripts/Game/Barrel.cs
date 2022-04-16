00f2a199391b34bd0d3af0db47a50cd4d243b5f7
resul <resul3734@gmail.com>
2022-04-17 00:49:09 +0300

    created a fragmented version of the red box

--- Assets/Scripts/Game/Barrel.cs
+++ Assets/Scripts/Game/Barrel.cs
@@ -1,14 +1,17 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace Game
 {
+    
     public class Barrel : MonoBehaviour
-    { // sol collector ve sol trail konumu yanlış ve kamera yükselmesini hallet!
+    {
         public Collector collector;
         public Material greenMat;
+        public GameObject brokenRock, brokenRockEffect;
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Stairs"))
@@ -27,5 +30,22 @@ namespace Game
             GetComponentInChildren<Animation>().enabled = false;
             transform.rotation = new Quaternion(0f,0f,0f,0f);
         }
+        
+        private IEnumerator BoxExplosions()
+        {
+            yield return new WaitForSeconds(1.5f);
+            Instantiate(brokenRockEffect, transform.position,Quaternion.identity);
+            GameObject brokenRockObject = Instantiate(brokenRock, transform.position,Quaternion.identity) as GameObject;
+            Rigidbody[] allRigidBodies = brokenRockObject.GetComponentsInChildren<Rigidbody>();
+            if (allRigidBodies.Length > 0)
+            {
+                foreach (var body in allRigidBodies)
+                {
+                    body.AddExplosionForce(500, transform.position,1);
+                }
+            }
+            Destroy(this.gameObject);
+        }
+        
     }
 }
